ce4bc17363dd1d6ccd42ede451d6365388e10704
JonathanZul <126488992+JonathanZul@users.noreply.github.com>
2025-09-05 23:42:15 -0300

    feat: Create 'What's New homepage section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/home/Hero';
+import WhatsNew from '@/components/home/WhatsNew';
 
 export default function Home() {
   return (
     <>
       <Hero />      
+      <WhatsNew />
     </>
   );
 }
